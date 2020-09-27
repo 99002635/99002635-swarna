ed137dce603c7974f345ab8258f4ec9ae4527ebb
99002635 <71486865+99002635@users.noreply.github.com>
2020-09-27 17:35:55 +0530

    Update main.c

--- 3. IMPLEMENTATION/main.c
+++ 3. IMPLEMENTATION/main.c
@@ -1,1 +1,48 @@
+#include <header.h>
 
+
+void main()
+ {
+   char ans;
+   int ch;
+   screenheader();
+   intro();
+   do
+     {
+       screenheader();
+       printf("\n\n\n                      Choose a category:\n");
+       printf("                        1. Get availability\n");
+       printf("                     2. Features of room\n");
+       printf("                        3. Room allocation\n");
+       printf("           4. Show customer details\n");
+       printf("           5. Restaurant\n");
+       printf("                        6. Cancellation\n");
+       printf("                        7. Room Deallocation\n");
+       printf("                 8. Exit\n");
+       scanf("%d",&ch);
+       switch(ch)
+   {
+     case 1:getavail();
+     putavail();
+     break;
+     case 2:features();
+     break;
+     case 3:allocate();
+     break;
+     case 4:putcust();
+     break;
+     case 5:restaurant();
+     break;
+     case 6:cancel();
+     break;
+     case 7:deallocate();
+     break;
+     case 8:exit(0);
+     default:printf("\n\n\nWrong choice!!!!\n\nPlease choose 1-6");
+      getch();
+   }
+ printf("\n\nDo you want to continue:");
+ fflush(stdin);
+ scanf("%c",&ans);
+     }while(ans=='y'||ans=='Y');
+ }
