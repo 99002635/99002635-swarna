43864e3b6de063985f18352621aa1ba17643c49f
99002635 <71486865+99002635@users.noreply.github.com>
2020-09-27 17:34:34 +0530

    Update header.h

--- 3. IMPLEMENTATION/inc/header.h
+++ 3. IMPLEMENTATION/inc/header.h
@@ -2,3 +2,14 @@
 #include<stdio.h>
 #include<string.h>
 #include<process.h>
+
+void getavail();
+void putavail();
+void features();
+void allocate();
+void putcust();
+void restaurant();
+void deallocate();
+void cancel();
+void intro();
+void screenheader();
